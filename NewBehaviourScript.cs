a4e4c6b504c17b2a41f5013e25dd66146cb120c8
zelaya <37353767+gabrielzelaya@users.noreply.github.com>
2021-05-12 21:50:15 -0400

    Add files via upload
    scrip para el jugador, cuenta los puntos

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GanasPuntos : MonoBehaviour
+{
+    private int score;
+    public Text scoreText;
+    public Text ganaste;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0
+        scoreText.text = "PUNTOS : " + score.ToString();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy"){
+            Destroy(collision.gameObject);
+            score +=10;
+            scoreText.text = "PUNTOS : " + score.ToString();
+
+            if (score >= 30)
+
+                ganaste.text = "GANASTE";
+                
+        }
+
+    }
+}
+/* 
+⣠⠴⠒⢛⣋⣉⣉⣉⣉⣛⠓⠦⣤⣀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣠⠴⢚⡭⠖⠚⠉⠉⠁⠀⠀⠀⠀⠉⠉⠓⠲⢭⡓⢦⡀⠀⠀⠀
+⠀⠀⠀⠀⣠⢞⡥⠚⠉⠀⠀⠀⠀ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠳⡝⢦⠀
+⠀⠀⠀⣰⣫⠞⡤⡀⠀⢀⠀⠀⠀⠀⡀⠀⡀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠘⣎⢧
+⠀⠀⢰⣷⠇⠈⠛⣵⠄⠀⢁⡆⠀⢠⠇⠸⢼⡀⠾⠻⠢⡄⠀⠀⠀⠀⠀⠘⡾⡆
+⠀⠀⡏⡼⠀⠀⣼⠏⠀⢠⣾⠀⢀⡞⣳⡀⠈⣧⠀⠀⠀⢹⡄⢀⡶⣛⣓⣦⢹⣷
+⠀⢸⠁⡇⠀⣸⠁⠀⣠⢯⡇⢀⣾⣀⠀⢳⡄⢻⡄⠀⠀⠈⣿⣸⣸⠁⠀⢸⠈⢹
+⠀⢸⠀⡇⢠⠇⢀⡜⢁⣟⡴⠋⠀⠉⠉⠓⠻⡜⡇⠀⢀⣖⡿⠿⠋⠀⠀⠋⠉⢻
+⠀⢿⠀⣇⢸⡞⠁⢠⡾⠋⠀⠀⠀⠀⠀⠀⠀⢻⡇⠀⠸⣟⠁⠀⠀⡟⢦⠀⠀⣸
+⠀⢸⠀⠹⡾⡯⠿⠦⡄⠀⠀⠀⢀⣤⣦⣄⡀⠀⣧⠀⠀⠈⡷⠆⠀⣇⠸⡆⢾⡷
+⠀⢸⠀⠀⡇⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠃⣿⠀⠀⠸⣇⠀⢀⣿⠀⠹⣾⠃
+⠀⢸⠀⠀⡇⣿⠀⠀⠘⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⢸⣿⣿⠉⢸⠀⠀⢙⠀
+⠀⠈⡇⠀⢧⢻⡀⠀⠀⣀⠀⠀⠀⣀⠀⠀⠀⠀⣿⠀⠀⢸⠉⢸⠀⠈⡇⠀⠀⢧
+⠀⠀⢹⠀⢸⠈⢷⣄⠀⠈⠃⠘⠉⠁⠀⠀⠀⣠⣿⠀⠀⡏⠀⠘⡀⠀⢿⠀⠀⠈
+⠀⠀⠀⢇⠀⢇⠀⠙⠳⣤⡀⠀⢀⣠⡤⢖⣋⡥⡇⠀⣸⠀⠀⠀⣇⠀⢹⡇⠀⠀
+⠀⠀⠀⠈⢇⠘⣄⠀⠀⠀⠉⢩⢿⣷⣿⡉⠀⢸⠇⢸⠟⢳⠤⠤⠼⣄⡘⣿⡀⠀
+⠀⠀⠀⠀⠈⣧⡙⣴⠋⠉⠉⣻⣾⣇⣿⣧⠀⣼⣰⠃⣰⠋⢀⡤⠖⠂⠈⢳⣧⠀
+⠀⠀⠀⢠⡞⣫⠽⢿⣇⡴⡏⢻⣿⣿⣟⣟⣿⡷⠃⣰⠃⡴⠋⠀⠀⠀⠀⠀⣿⢧
+⠀⠀⠀⢸⡇⠐⠚⠡⣿⢧⠁⠀⠛⢻⠄⡏⠞⠀⢰⢇⡞⠁⠀⠀⢀⠤⡀⠀⣿⠘
+⠀⠀⠀⣸⡇⠀⠀⢸⡏⠈⠀⠀⠀⢸⣶⠛⢦⠴⣻⡞⣀⣀⣰⠮⠶⠚⠁⠀⣿⡄
+⠀⢀⡏⡿⢧⡀⠀⠀⣧⠀⠀⢀⣠⠞⣿⣤⠞⢰⠋⠉⠁⠀⠀⠀⢀⣤⠀⢸⡏⢧
+⠀⢸⠁⠙⢦⣙⠦⠼⣿⡖⠚⣉⣠⠖⠁⠸⡄⡿⠀⠀⠀⠀⠠⠚⠉⠀⠀⡞⡇⢸ Easter egg
+⠀⡟⠀⠀⠀⠎⡍⠉⢹⢿⡏⠁⠀⠀⠀⠀⢹⡅⠀⠀⠀⠀⠀⠀⠀⠀⡼⢡⣷⠀
+⡼⠃⠀⠀⠀⠆⡇⢀⠟⡆⠀⠀⠀⠀⠀⠀⠀⢷⠀⠀⠀⠀⠀⠀⠀⣸⠁⢸⢻⠀
+⠁⠀⠀⠀⠀⠀⠁⡾⠀⢹⡘⠃⠀⠀⠀⠀⠀⠈⢣⢤⡀⠀⠀⠀⢠⠇⠀⢸⢸⡆
+⠀⠀⠀⠀⠀⠀⢾⡇⠀⠀⢳⡀⠀⠀⠀⠀⠀⠀⠀⢧⠙⠆⠀⢠⠏⠀⠀⡟⠀⡇
+⠀⠀⠀⠀⠀⠀⢸⠁⠀⠀⠀⢷⠀⠀⠀⠀⠀⠀⠀⠈⠁⠀⢠⠏⠀⠀⠀⡇⠀⡇
+
+*/
